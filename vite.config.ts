2fef41545c89b43542b1a5611b002b8c4e86e64c
Osman Aydoğan <205541061osmanaydogan@gmail.com>
2025-04-21 07:57:10 +0300

    GitHub Pages için base ayarı yapıldı ve docs klasörü eklendi

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/PuzzleCans_Websitee/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
